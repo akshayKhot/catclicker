fd4a68f6d8eaabe920552a98c6ff1df464e08aa2
Akshay Khot <akshay.7277@gmail.com>
2016-02-13 17:33:59 -0800

    implement counter functionality
    count clicks on each cat image independently of other cats

--- count.js
+++ count.js
@@ -3,14 +3,37 @@
  */
 
 var cats = {
-    "Fossie": "https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg",
-    "Bootsie": "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
-    "Meow": "https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg",
-    "Ruby": "http://breadedcat.com/wp-content/uploads/2012/02/cat-breading-tutorial-004.jpg",
-    "Genie": "http://i3.mirror.co.uk/incoming/article99763.ece/ALTERNATES/s615/a-bengal-cat-935179217.jpg"
+    "Fossie": {
+        name: "Fossie",
+        url: "https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg",
+        count: 0
+    },
+    "Bootsie": {
+        name: "Fossie",
+        url: "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
+        count: 0
+    },
+    "Meow": {
+        name: "Fossie",
+        url: "https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg",
+        count: 0
+    },
+    "Ruby": {
+        name: "Fossie",
+        url: "http://breadedcat.com/wp-content/uploads/2012/02/cat-breading-tutorial-004.jpg",
+        count: 0
+    },
+    "Genie": {
+        name: "Fossie",
+        url: "http://images4.fanpop.com/image/photos/16000000/Beautiful-Cat-cats-16096437-1280-800.jpg",
+        count: 0
+    },
 }
 
 var catNodes = document.getElementsByClassName("cat");
+var countNode = document.getElementById("count");
+var catImg = document.getElementById("catImg");;
+
 for(var i=0; i<catNodes.length; i++) {
     catNodes[i].addEventListener("click", function() {
         //set the name for the cat
@@ -18,10 +41,22 @@ for(var i=0; i<catNodes.length; i++) {
         document.getElementById("name").innerHTML = catName;
 
         //set the image
-        var catImg = document.getElementById("catImg");
-        catImg.setAttribute("src", cats[catName]);
-    })
+        catImg.setAttribute("src", cats[catName].url);
+
+        //reset counter to 0
+        countNode.textContent = cats[catName].count;
+    });
 }
+
+catImg.addEventListener("click", function() {
+    //Get the text from the name tag
+    if(document.getElementById("name").textContent !== ""){
+        var catName = document.getElementById("name").textContent;
+        countNode.textContent = ++cats[catName].count;
+    }
+});
+
+
 
 
 
